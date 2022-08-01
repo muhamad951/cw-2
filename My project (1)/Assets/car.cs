f1cad196c4333d70911d8e807b74592f125f4231
muhamad <muhamadmic@outlook.com>
2022-08-01 22:01:30 +0300

    cw2
    ترا يمكن في بعض الاخطاء ما عرفت اسويهم

---
+++ My project (1)/Assets/car.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour
+{
+    string hero name="aisha";
+int hero height=150;
+int hero age=15;
+string hero super power = "flying";
+    string villain name="lulu";
+int villain height=155;
+int villain age=19;
+string villain super power = "laser eyes";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int ageDifference = hero age + villain age;
+        print("hero name is" + hero name + "hero age is" + hero age + "hero height is" + hero height + "hero super power" + hero super power);
+        print("villain Name" + villain Name + "villain height" + villain height + "villain age" + villain age + "villain super power" + villain super power);
+        print("age difference is" + ageDifference);
+        heroHeight = +5
+  print(hero height);
+        villainSuperPower = "none";
+        print("villainSuperPower after" + villainSuperPower);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
